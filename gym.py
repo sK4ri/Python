67da2b76958ede90ffac009c62db08a961409973
sK4ri <skari.simon@gmail.com>
2018-10-30 16:17:09 +0100

    battle4

--- gym.py
+++ gym.py
@@ -6,19 +6,18 @@ while True :
         print('You are underage!')
         break
     elif q1 == 'yes' :
-        q2 = input(questions[1])        
+        q2 = input(questions[1])
         if q2 == 'yes' :
             print('Price: 1500HUF')
             break
         elif q2 == 'no' :
-            q3 = input(questions[2])            
+            q3 = input(questions[2])
             if q3 == 'yes' :
                 print('Price: 300HUF')
                 break
             elif q3 == 'no' :
-                q4 = input(questions[3])               
+                q4 = input(questions[3])
                 if q4 == 'yes' :
-
                     print('Price: 750HUF')
                     break
                 elif q4 == 'no' :
